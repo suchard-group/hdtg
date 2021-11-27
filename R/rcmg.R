f68d3073ddb72307a7a5e0d8f18b3528f7b24c40
Zhenyu Zhang <zhangzhenyusa@gmail.com>
2021-11-26 16:07:28 -0800

    add comparison with other tmvn pkgs

--- R/rcmg.R
+++ R/rcmg.R
@@ -5,7 +5,7 @@
 #' @param n number of samples
 #' @param mean a d-dimensional mean vector
 #' @param prec a d-by-d precision matrix of the Gaussian distribution
-#' @param constraits a (list?) of constraints
+#' @param constraits a d-dimensional vector where a positive (negative) number means >0 (<0) truncation.
 #' @param p0 a d-dimensional vector of the initial value. It must satisfy all constraints. If not specified a random initial value will be used
 #' @param burnin the number of burn-in iterations
 #' @param t time length to simulate the Markov process
@@ -15,7 +15,7 @@
 #'
 #' @examples rcmg(1,1,1,1,1)
 rcmg <- function(n, mean, prec, constraits, t, burnin, p0 = NULL) {
-  debug_flg = F
+  debug_flg = T
   stopifnot("n > burnin must be integers!" = (n %% 1 == 0 && burnin %% 1 == 0 && n > burnin))
   stopifnot("mean and prec must be numeric" = (is.numeric(mean) && is.numeric(prec)))
   # TODO add other checks for arguments. all dimensions must match.
@@ -40,7 +40,7 @@ rcmg <- function(n, mean, prec, constraits, t, burnin, p0 = NULL) {
     p0 <- hzz(get_prec_product, mean, p0, constraits, momentum, t_jittered)
     samples[, i] <- p0
     if (debug_flg) {
-      cat('----------------------one iteration done----------------------')
+      cat('iteration', i, 'done \n')
     }
   }
   return(samples)
