4685f09386597a13cb79fd9d6924287edda6f53f
Andrew Chin <chinandrew96@gmail.com>
2022-02-22 00:17:34 -0500

    Add const to arguments

--- src/EigenMatVec.cpp
+++ src/EigenMatVec.cpp
@@ -15,25 +15,25 @@ using std::atan2;
 // }
 
 // [[Rcpp::export]]
-Rcpp::List RcppHamiltonian(Map<VectorXd> position, Map<VectorXd> momentum, double time) {
+Rcpp::List RcppHamiltonian(const Map<VectorXd> position, const Map<VectorXd> momentum, const double time) {
   return Rcpp::List::create(Rcpp::_["position"]=momentum * sin(time) + position * cos(time), 
                             Rcpp::_["momentum"]=momentum * cos(time) - position * sin(time));
 }
 
 // [[Rcpp::export]]
-VectorXd RcppBounceMomentum(Map<VectorXd> position,
-                            Map<VectorXd> momentum,
-                            Map<MatrixXd> constraint_direc,
-                            Map<MatrixXd> constraint_row_normsq,
-                            int bounce_idx) {
+VectorXd RcppBounceMomentum(const Map<VectorXd> position,
+                            const Map<VectorXd> momentum,
+                            const Map<MatrixXd> constraint_direc,
+                            const Map<MatrixXd> constraint_row_normsq,
+                            const int bounce_idx) {
   return momentum - 2 * momentum.dot(constraint_direc.row(bounce_idx-1)) /  constraint_row_normsq(bounce_idx-1) * constraint_direc.row(bounce_idx-1).transpose();
 }
 
 // [[Rcpp::export]]
-Rcpp::List RcppBounceTime(Map<VectorXd> position,
-                          Map<VectorXd> momentum,
-                          Map<MatrixXd> constraint_direc,
-                          Map<VectorXd> constraint_bound) {
+Rcpp::List RcppBounceTime(const Map<VectorXd> position,
+                          const Map<VectorXd> momentum,
+                          const Map<MatrixXd> constraint_direc,
+                          const Map<VectorXd> constraint_bound) {
   
   ArrayXd fa = (constraint_direc * momentum).array();
   ArrayXd fb = (constraint_direc * position).array();
