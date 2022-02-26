9ea397b34e911709fde5bedf8d0c9af04a05d699
Andrew Chin <chinandrew96@gmail.com>
2022-02-25 19:20:48 -0500

    fix indentation

--- src/EigenMatVec.cpp
+++ src/EigenMatVec.cpp
@@ -5,31 +5,30 @@ using Eigen::Map;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using Eigen::ArrayXd;
-using std::atan2;
 
 
 std::pair<VectorXd, VectorXd> Hamiltonian(const VectorXd position, 
-                                              const VectorXd momentum, 
-                                              const double time) {
+                                          const VectorXd momentum, 
+                                          const double time) {
   return std::make_pair(momentum * sin(time) + position * cos(time), 
                         momentum * cos(time) - position * sin(time));
 }
 
 
 VectorXd ReflectMomentum(const VectorXd position,
-                             const VectorXd momentum,
-                             const Map<MatrixXd> constraint_direc,
-                             const Map<VectorXd> constraint_row_normsq,
-                             const int bounce_idx) {
+                         const VectorXd momentum,
+                         const Map<MatrixXd> constraint_direc,
+                         const Map<VectorXd> constraint_row_normsq,
+                         const int bounce_idx) {
   return momentum - 2 * momentum.dot(constraint_direc.row(bounce_idx-1)) /  
     constraint_row_normsq(bounce_idx-1) * constraint_direc.row(bounce_idx-1).transpose();
 }
 
 
 std::pair<double, int> BounceTime(const VectorXd position,
-                                      const VectorXd momentum,
-                                      const Map<MatrixXd> constraint_direc,
-                                      const Map<VectorXd> constraint_bound) {
+                                  const VectorXd momentum,
+                                  const Map<MatrixXd> constraint_direc,
+                                  const Map<VectorXd> constraint_bound) {
   
   ArrayXd fa = (constraint_direc * momentum).array();
   ArrayXd fb = (constraint_direc * position).array();
@@ -62,9 +61,9 @@ VectorXd GenerateWhitenedSample(const Map<VectorXd> initial_position,
   double travelled_time = 0;
   while (true) {
     std::pair<double, int> bounce = BounceTime(position,
-                                                   momentum,
-                                                   constraint_direc,
-                                                   constraint_bound);
+                                               momentum,
+                                               constraint_direc,
+                                               constraint_bound);
     if (bounce.first < total_time - travelled_time) {
       double bounce_time = bounce.first;
       std::pair<VectorXd, VectorXd> hamiltonian = Hamiltonian(position, momentum, bounce_time);
@@ -104,9 +103,9 @@ VectorXd WhitenPosition(const Map<VectorXd> position,
 
 // [[Rcpp::export]]
 VectorXd UnwhitenPosition(const Map<VectorXd> position,
-                        const Map<MatrixXd> cholesky,
-                        const Map<VectorXd> mean,
-                        bool precision) {
+                          const Map<MatrixXd> cholesky,
+                          const Map<VectorXd> mean,
+                          bool precision) {
   
   if (precision) {
     return cholesky.triangularView<Eigen::Upper>().solve(position) + mean;
