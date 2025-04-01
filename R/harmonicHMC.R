cf32d8e70abf8c4dbece6b7d872db87a3ba24d00
Zhenyu Zhang <zhzhang@alnylam.com>
2025-04-01 15:13:08 -0400

    avoid bad naming

--- R/harmonicHMC.R
+++ R/harmonicHMC.R
@@ -9,8 +9,8 @@
 #' @param mean a d-dimensional mean vector.
 #' @param choleskyFactor upper triangular matrix R from Cholesky decomposition of 
 #' precision or covariance matrix into R^TR. 
-#' @param F F matrix (k-by-d matrix where k is the number of linear constraints). 
-#' @param g g vector (k-dimensional). 
+#' @param constrainDirec the k-by-d F matrix (k is the number of linear constraints). 
+#' @param constrainBound the k-dimensional g vector. 
 #' @param init a d-dimensional vector of the initial value. `init` must satisfy all constraints. 
 #' @param time HMC integration time for each iteration. Can either be
 #' a scalar value for a fixed time across all samples, or a length 2 vector of a
@@ -35,18 +35,18 @@
 #' Sigma <- t(A) %*% A
 #' R <- cholesky(Sigma)
 #' mu <- rep(0, d)
-#' F <- diag(d)
-#' g <- rep(0,d)
+#' constrainDirec <- diag(d)
+#' constrainBound <- rep(0,d)
 #' initial <- rep(1, d)
-#' results <- harmonicHMC(1000, 1000, mu, R, F, g, initial, precFlg = FALSE)
+#' results <- harmonicHMC(1000, 1000, mu, R, constrainDirec, constrainBound, initial, precFlg = FALSE)
 #' @references
 #' \insertRef{pakman2014exact}{hdtg}
 harmonicHMC <- function(nSample,
                         burnin = 0,
                         mean,
                         choleskyFactor,
-                        F,
-                        g,
+                        constrainDirec,
+                        constrainBound,
                         init,
                         time = c(pi / 8, pi / 2),
                         precFlg,
@@ -58,24 +58,24 @@ harmonicHMC <- function(nSample,
   if (time[2] < time[1]) {
     stop("Upper bound for integration time must be greater than lower bound.")
   }
-  if (sum(F %*% init + g > 0) < length(g)) {
+  if (sum(constrainDirec %*% init + constrainBound > 0) < length(constrainBound)) {
     stop("Initial value x does not satisfy Fx + g >=0")
   }
-  samples <- matrix(nrow = nSample, ncol = ncol(F))
+  samples <- matrix(nrow = nSample, ncol = ncol(constrainDirec))
   randomBounceTime <-
     ifelse(length(time) == 2, TRUE, FALSE)
   numBounces <- vector(mode = "integer", length = nSample)
   diagnosticMode <- "bounceDistances" %in% extraOutputs
   bounceDistances <- vector(mode = "list",
                             length = ifelse(diagnosticMode, nSample, 0))
-  whitenedConstraints <- applyWhitenTransform(F,
-                                              g,
+  whitenedConstraints <- applyWhitenTransform(constrainDirec,
+                                              constrainBound,
                                               choleskyFactor,
                                               mean,
                                               precFlg)
   position <- whitenPosition(init,
-                             F,
-                             g,
+                             constrainDirec,
+                             constrainBound,
                              choleskyFactor,
                              mean,
                              precFlg)
@@ -83,7 +83,7 @@ harmonicHMC <- function(nSample,
     set.seed(seed)
   }
   for (i in 1:(nSample + burnin)) {
-    momentum <- rnorm(ncol(F))
+    momentum <- rnorm(ncol(constrainDirec))
     results <- simulateWhitenedDynamics(
       position,
       momentum,
