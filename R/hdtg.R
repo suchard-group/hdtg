8d6ad25cf4da2330b70f805c5c19886ec6d12b22
Zhenyu Zhang <zhangzhenyusa@gmail.com>
2025-12-14 23:57:07 -0500

    remove unused files

--- R/hdtg.R
+++ R/hdtg.R
@@ -1,7 +1,6 @@
 #' @useDynLib hdtg, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #' @importFrom RcppParallel RcppParallelLibs
-#' @importFrom RcppXsimd supportsSSE supportsAVX
 #' @importFrom stats rnorm runif
 #' @importFrom Rdpack reprompt
 NULL
